766676514fdd1011ee63ec876a3b268241eb28ea
Ryan <violva317018@gmail.com>
2023-09-07 21:17:28 +0800

    部屬網站

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/my-personal-website/" //為github的專案名稱
-      : "/",
-};
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
