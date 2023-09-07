c648b058554d33476c9de062807d08936271d189
Ryan <violva317018@gmail.com>
2023-09-07 20:40:21 +0800

    部屬測試

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+# abort on errors
+set -e
+ 
+# build
+npm run build
+ 
+# navigate into the build output directory
+// 因為 npm run build 出來的檔案預設是會被 gitignore 忽略的，所以要手動切換目錄
+cd dist
+ 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+ 
+git init
+git add -A
+git commit -m 'deploy'
+ 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+ 
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/violva317018/my-personal-website.git master:gh-pages
+ 
+cd -
